b832e9aaf9178e8ad89ccd299ee00700c44fd8d4
BuildTools <unconfigured@null.spigotmc.org>
2024-04-03 13:17:49 -0500

    Initial commit

---
+++ src/main/java/net/sleepykairo/piratemod/PirateModClient.java
@@ -0,0 +1,10 @@
+package net.sleepykairo.piratemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PirateModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
