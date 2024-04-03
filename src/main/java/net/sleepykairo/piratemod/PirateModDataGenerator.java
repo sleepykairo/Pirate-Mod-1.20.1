643b7d24877cf285e0bcda4d8842b63918205f79
BuildTools <unconfigured@null.spigotmc.org>
2024-04-03 14:43:52 -0500

    datagen added lololol

--- src/main/java/net/sleepykairo/piratemod/PirateModDataGenerator.java
+++ src/main/java/net/sleepykairo/piratemod/PirateModDataGenerator.java
@@ -2,10 +2,18 @@ package net.sleepykairo.piratemod;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.data.DataGenerator;
+import net.sleepykairo.piratemod.datagen.*;
 
 public class PirateModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
